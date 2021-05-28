269323153f6527d535ade10bf6d586dacf2271e7
Danteark <49062845+Danteark@users.noreply.github.com>
2021-05-28 14:38:52 +0530

    Initial Commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#add 
+def add(x,y):
+    return x+y
+#sub
+def subtract(x,y):
+    return x-y
+#mult
+def multiply(x,y):
+    return x*y		#on Bug456
+#div
+def divide(x,y):
+    if y=0:
+    	return DIVIDE_BY_ZERO
+    else
+    	return x/y
+
+#square implementation
+def square(x)
+	pass
